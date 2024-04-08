b243926ec935c80ae981bf2b3b76f2b7a398c8b5
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-04-09 01:36:19 +0200

    NEW - Initial commit

---
+++ src/components/StepLoading.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Spinner } from "flowbite-svelte"
+</script>
+  
+<div class="flex justify-center items-center flex-col gap-y-2">
+    <Spinner color="purple" size={6}/>
+    <p class="opacity-75">Subiendo archivo y extrayendo texto...</p>
+</div>
